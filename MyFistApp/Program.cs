a6c972e75d795ee7801a718905c6c585de28c16d
antondanilov <veymz321@gmail.com>
2024-02-01 17:23:52 +0700

    Добавьте файлы проекта.

---
+++ MyFistApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyFistApp;
+{
+   class Program
+   { 
+       static void Main(string[] agrs)
+       {
+            Console.WriteLine("Hello, World!");
+       }
+   }
+}
